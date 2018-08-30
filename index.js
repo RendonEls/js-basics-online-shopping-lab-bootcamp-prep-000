fd3108f75ce28a1c8250f206a420dee67cebe7df
Rendon Elsesser <rendonelsesser@gmail.com>
2018-08-30 01:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,8 +67,10 @@ cartTotal += cart[i].itemPrice
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if (cart[i].itemName == item){
-      
+      cart.splice(i,1)
+      return cart
     }
+    return "That item is not in your cart."
   }
 }
 
