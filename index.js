af1691388b39411d09076e302bfd925349baa9b6
Rendon Elsesser <rendonelsesser@gmail.com>
2018-08-30 01:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === null){
+  if (cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you."
   }
   var orderTotal = total()
